fcb824a517311ec3f7910bd2501b1cdbbddf64af
beqaperanidze <beqaperanidze28@gmail.com>
2025-05-17 10:49:17 +0400

    controllers finished

---
+++ CoffeeBeanExplorer/Models/DTOs/UserListDto.cs
@@ -0,0 +1,33 @@
+﻿using CoffeeBeanExplorer.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeBeanExplorer.Models.DTOs;
+
+public class UserListDto
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public int BeanId { get; set; }
+    public string BeanName { get; set; } = string.Empty;
+    public CollectionType Type { get; set; }
+    public string CollectionTypeName => Type.ToString();
+    public DateTime CreatedAt { get; set; }
+}
+
+public class CreateUserListDto
+{
+    [Required]
+    public int UserId { get; set; }
+    
+    [Required] 
+    public int BeanId { get; set; }
+
+    [Required] 
+    public CollectionType Type { get; set; }
+}
+
+public class UpdateUserListDto
+{
+    [Required] public CollectionType Type { get; set; }
+}
