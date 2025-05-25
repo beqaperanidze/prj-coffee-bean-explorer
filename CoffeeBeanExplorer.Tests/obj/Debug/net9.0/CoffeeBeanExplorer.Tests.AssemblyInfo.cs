c8091d4c7064598c470ecaf05f8727392570a8fc
beqa peranidze <beqaperanidze28@gmail.com>
2025-05-25 16:19:17 +0400

    Updated .gitignore

--- CoffeeBeanExplorer.Tests/obj/Debug/net9.0/CoffeeBeanExplorer.Tests.AssemblyInfo.cs
+++ CoffeeBeanExplorer.Tests/obj/Debug/net9.0/CoffeeBeanExplorer.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeBeanExplorer.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ef543e76aa31ce0250662973214198b6e78898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734cb38a53976a6b2527c8510fbbb5203347cb6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeBeanExplorer.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeBeanExplorer.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
